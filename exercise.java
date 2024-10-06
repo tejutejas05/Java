dc74fbc2a643f02afe557210a07f11e3c451b0bc
tejutejas05 <teju05tejas@gmail.com>
2024-10-06 22:13:09 +0530

    Comment

--- exercise.java
+++ exercise.java
@@ -1,24 +1,25 @@
-//package Exceptionexercise;
-public class exercise {
-    // public static int dividenum(int dividend,int divisor){
-    //     if(divisor==0){
-    //         throw new ArithmeticException("cannot divided by zero");
-    //     }
-    //     return dividend/divisor;
-    // }
-    public static void main(String args[]){
-        try{
-         //   int res=dividenum(5, 0);
-            int n=10;
-            int k=0;
-            if(k==0){
-                throw new ArithmeticException("cannot divided by zero");
-            }
-            int res=n/k;
-            System.out.println("result:"+res);
-        }
-        catch(ArithmeticException e){
-            System.out.println(e);
-        }
-    }
-}
+// //package Exceptionexercise;
+// public class exercise {
+//     // public static int dividenum(int dividend,int divisor){
+//     //     if(divisor==0){
+//     //         throw new ArithmeticException("cannot divided by zero");
+//     //     }
+//     //     return dividend/divisor;
+//     // }
+//     public static void main(String args[]){
+//         try{
+//          //   int res=dividenum(5, 0);
+//             int n=10;
+//             int k=0;
+//             if(k==0){
+//                 throw new ArithmeticException("cannot divided by zero");
+//             }
+//             int res=n/k;
+//             System.out.println("result:"+res);
+//         }
+//         catch(ArithmeticException e){
+//             System.out.println(e);
+//         }
+//     }
+// }
+// Hello 
